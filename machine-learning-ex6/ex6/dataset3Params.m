28c2bd80291641e00fd07a06ecd47e4861ade5c8
liutaotao <lttzzlll@gmail.com>
2017-01-14 20:43:06 +0800

    svm practice completed

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,11 +23,22 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+C_vec = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+sigma_vec = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+minist_error = 1e10;
 
-
-
-
-
+for i = 1: size(C_vec, 1),
+    for j = 1: size(sigma_vec, 1),
+        model= svmTrain(X, y, C_vec(i), @(x1, x2) gaussianKernel(x1, x2, sigma_vec(j)));
+        predictions = svmPredict(model, Xval);
+        error =  mean(double(predictions ~= yval));
+        if (error < minist_error),
+            minist_error = error;
+            C = C_vec(i);
+            sigma = sigma_vec(j);
+        end;
+    end;
+end;
 
 % =========================================================================
 
