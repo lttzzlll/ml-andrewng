862fc50084ee868801fbbd4bbe84b64b86bcbeca
liutaotao <lttzzlll@gmail.com>
2017-03-10 22:27:04 +0800

    logistic regression

--- recode/machine-learning-ex2/ex2/costFunction.m
+++ recode/machine-learning-ex2/ex2/costFunction.m
@@ -20,10 +20,10 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
-
-
-
-
+h = sigmoid(X * theta);
+res = -y' * log(h) - (1-y') * log(1-h);
+J = res / m;
+grad = X' * (h - y) / m;
 
 
 
